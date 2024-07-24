298d1d1eaa47f87c39b7b6ba789b5f2bb0e81cd2
Keith Taylor <k-taylor@outlook.com>
2024-07-24 11:40:19 +0100

    added module level documentation. Also fixed indentation on divide by zero example (now passes doctest).

--- calculator/calculations.py
+++ calculator/calculations.py
@@ -1,27 +1,43 @@
 #  calc functions
 
 """
-Takes two numbers and an operator then returns a float value result.
-Args:
-    a, b 
+Provides simple math calculations on two input variables (as either float or int).
+
+The module conatins the following functions: 
+
+    - `add(a,b)` -- returns the sum of two numbers. 
+    - `subtract(a,b)` -- returns the difference of two numbers. 
+    - `multiply(a,b)` -- returns the product of two numbers. 
+    - `divide(a,b)` -- returns the quotent of two numbers. 
+
+Examples:
+    >>> from calculator import calculations
+    >>> calculations.add(4,2)
+    6.0
+    >>> calculations.multiply(2.0, 4.0)
+    8.0
+    >>> from calculator.calculations import divide
+    >>> divide(4.0, 2)
+    2.0
+
 """
 
 def add(a:float|int, b:float|int) -> float:
     """
-    Compute and return the sum of two numbers.
+    Computes and returns the sum of two numbers.
     
     Args:
-    a : the first operand
-    b : the second operand
-    
+        a : the first operand
+        b : the second operand
+      
     Returns:
-    The sum of the two inputs. 
+        The sum of the two inputs. 
 
     Examples:
-    >>> add(4, 2)
-    6.0
-    >>> add(4.0, 2.0)
-    6.0
+        >>> add(4, 2)
+        6.0
+        >>> add(4.0, 2.0)
+        6.0
     """
     
     return float(a+b) 
@@ -31,17 +47,18 @@ def subtract(a:float|int, b:float|int) -> float:
     Compute and return the difference of two numbers.
     
     Args:
-    a : the first operand
-    b : the second operand
+        a : the first operand
+        b : the second operand
     
     Returns:
-    The difference between the first and second input. 
+        The difference between the first and second input. 
     
     Examples: 
-    >>> subtract(4, 2)
-    2.0
-    >>> subtract(4.0, 2.0)
-    2.0
+        >>> subtract(4, 2)
+        2.0
+        >>> subtract(4.0, 2.0)
+        2.0
+        
     """
     return float(a-b) 
 
@@ -50,18 +67,18 @@ def multiply(a:float|int, b:float|int) -> float:
     Compute and return the product of two numbers.
     
     Args:
-    a : the first operand
-    b : the second operand
+        a : the first operand
+        b : the second operand
     
     Returns:
-    The product of the two inputs. 
+        The product of the two inputs. 
 
     Examples: 
-    >>> multiply(4, 2)
-    8.0
-    >>> multiply(4.0, 2.0)
-    8.0
-    """
+        >>> multiply(4, 2)
+        8.0
+        >>> multiply(4.0, 2.0)
+        8.0
+        """
     return float(a*b) 
 
 def divide(a:float|int, b:float|int) -> float:
@@ -69,26 +86,26 @@ def divide(a:float|int, b:float|int) -> float:
     Compute and return the quotient of two numbers.
     
     Args:
-    a : the first operand (numerator)
-    b : the second operand (divisor)
+        a : the first operand (numerator)
+        b : the second operand (divisor)
     
     Returns:
-    The sum of the two inputs. 
+        The quotent of the two inputs. 
     
     Raises:
-    ZeroDivisionError: Division by zero is not allowed!
+        ZeroDivisionError: An error occurs when trying to divide by zero. 
     
     Examples:
-    >>> divide(4, 2)
-    2.0
-    >>> divide(4.0, 2.0)
-    2.0
-    # >>> divide(4, 0)
+        >>> divide(4, 2)
+        2.0
+        >>> divide(4.0, 2.0)
+        2.0
+        >>> divide(4, 0)
         Traceback (most recent call last):
         ...
         ZeroDivisionError: division by zero
     """
-    
+
     if b == 0:
-        raise ZeroDivisionError("Division by zero is not allowed!")
+        raise ZeroDivisionError("division by zero")
     return float(a/b) 
